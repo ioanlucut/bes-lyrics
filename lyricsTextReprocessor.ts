2f70a7e5b060e9e15a37adcb6d7666c74558bd15
Ioan Lucut <ioan.lucut88@gmail.com>
2023-03-27 17:52:41 +0300

    Update formatting

--- lyricsTextReprocessor.ts
+++ lyricsTextReprocessor.ts
@@ -1,31 +1,70 @@
-import _ from 'lodash';
+import _, { endsWith } from 'lodash';
 import fs from 'fs';
 import path from 'path';
 
 const OUTPUT_DIR = './out';
 const EMPTY_STRING = '';
+const NEW_LINE = '\n';
+const CARRIAGE_RETURN = '\r';
 
-(async () => {
-  const normalizeContent = (fileContent: string) =>
-    _.trim(fileContent)
-      .replaceAll(' si ', ' și ')
-      .replaceAll(' isus ', ' Isus ')
-      .replaceAll('dumnezeu', 'Dumnezeu')
-      .replaceAll('doamne', 'doamne')
-      .replaceAll(' fara ', ' fără ')
-      .replaceAll(' tara ', ' țară ');
+const trimContent = (fileContent: string) => _.trim(fileContent);
 
-  const filenames = fs.readdirSync(OUTPUT_DIR);
-
-  filenames.forEach((fileName) => {
+const reprocess = (strings: string[]) => {
+  strings.forEach((fileName) => {
     const filePath = path.join(path.join(__dirname, OUTPUT_DIR, fileName));
     const fileContent = fs.readFileSync(filePath).toString();
 
-    const maybeNewFilePath = normalizeContent(
-      filePath.replace('.md', '.txt').replace('BES 2023 ', EMPTY_STRING),
-    );
-    fs.renameSync(filePath, maybeNewFilePath);
+    const sections = fileContent
+      .split(/(\[.*])/gim)
+      .filter(Boolean)
+      .map(_.trim);
 
-    fs.writeFileSync(maybeNewFilePath, normalizeContent(fileContent));
+    const fileNameFromTheTitle =
+      !sections[1] || sections[1] === '[title]' ? sections[2] : sections[1];
+
+    const computedTitle = _.trim(
+      fileNameFromTheTitle
+        .replaceAll(':/', EMPTY_STRING)
+        .replaceAll('/:', EMPTY_STRING)
+        .replaceAll('.', EMPTY_STRING)
+        .replaceAll('!', EMPTY_STRING)
+        .replaceAll('?', EMPTY_STRING)
+        .replaceAll('„', EMPTY_STRING)
+        .replaceAll('”', EMPTY_STRING)
+        .replaceAll(':', EMPTY_STRING)
+        .replaceAll('  ', EMPTY_STRING)
+        .replaceAll(',', EMPTY_STRING)
+        .replaceAll('1X', EMPTY_STRING)
+        .replaceAll('1x', EMPTY_STRING)
+        .replaceAll('2X', EMPTY_STRING)
+        .replaceAll('2x', EMPTY_STRING)
+        .replaceAll('3X', EMPTY_STRING)
+        .replaceAll('3x', EMPTY_STRING)
+        .replaceAll('4X', EMPTY_STRING)
+        .replaceAll('4x', EMPTY_STRING),
+    );
+
+    const maybeNewFilePath = path.join(
+      __dirname,
+      OUTPUT_DIR,
+      `${computedTitle}.txt`,
+    );
+
+    const normalizedContent = _.trim(
+      _.flatten(
+        sections.map((section) => {
+          if (section.startsWith('[')) {
+            return [CARRIAGE_RETURN, section];
+          }
+          return section;
+        }),
+      ).join(NEW_LINE),
+    );
+
+    fs.writeFileSync(maybeNewFilePath, normalizedContent);
   });
+};
+
+(async () => {
+  reprocess(fs.readdirSync(OUTPUT_DIR));
 })();
