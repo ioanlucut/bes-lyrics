60bafdb02a90fb3cf5f00d15d543705fc25ba5e7
Ioan Lucut <ioan.lucut88@gmail.com>
2023-03-31 19:32:37 +0300

    Update the validator

--- lyricsTextValidator.ts
+++ lyricsTextValidator.ts
@@ -11,27 +11,29 @@ const ALLOWED_CHARS =
     EMPTY_STRING,
   );
 
-const verifyChars = (fileNames: string[]) =>
+const getUniqueChars = (fileNames: string[]) =>
   fileNames.map((fileName) => {
     const filePath = path.join(path.join(__dirname, OUTPUT_DIR, fileName));
 
-    return fs.readFileSync(filePath).toString();
+    return _.uniq(fs.readFileSync(filePath).toString());
   });
 
 // ---
 // RUN
 // ---
 
-const chars = _.flattenDeep(verifyChars(fs.readdirSync(OUTPUT_DIR)));
+const chars = _.flattenDeep(getUniqueChars(fs.readdirSync(OUTPUT_DIR)));
 const uniqueChars = _.uniq(chars).filter(Boolean).sort();
 const difference = _.difference(uniqueChars, ALLOWED_CHARS);
 
 if (!_.isEmpty(difference)) {
+  const ERROR_CODE = 1;
+
   console.log(
     `The difference between the allowed and found is: "${difference}"`,
   );
 
-  process.exit(1);
+  process.exit(ERROR_CODE);
 } else {
-  console.log(`The characters are valid.`);
+  console.log('All the characters are valid.');
 }
