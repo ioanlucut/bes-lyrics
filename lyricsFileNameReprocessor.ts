901a7d0985af462955c1019cdd7bc21a1cd8e8bf
Ioan Lucut <ioan.lucut88@gmail.com>
2023-04-13 15:18:50 +0300

    Update the file name reprocessor

--- lyricsFileNameReprocessor.ts
+++ lyricsFileNameReprocessor.ts
@@ -3,17 +3,18 @@ import fs from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
 import * as process from 'process';
-
-const EMPTY_STRING = '';
+import { EMPTY_STRING } from './constants';
 
 dotenv.config();
 
-const reprocessFileNames = (fileNames: string[]) => {
-  fileNames.forEach((fileName) => {
-    const filePath = path.join(process.env.VERIFIED_DIR, fileName);
-    const fileContent = fs.readFileSync(filePath).toString();
+const reprocessFileNames = (dir: string) => {
+  console.log(`"Processing ${dir}.."`);
 
-    const maybeNewFileName = _.trim(
+  fs.readdirSync(dir).forEach((fileName) => {
+    const filePath = path.join(dir, fileName);
+    const existingContent = fs.readFileSync(filePath).toString();
+
+    const newFileName = _.trim(
       fileName
         .replaceAll(':/', EMPTY_STRING)
         .replaceAll('/:', EMPTY_STRING)
@@ -28,27 +29,15 @@ const reprocessFileNames = (fileNames: string[]) => {
         .replaceAll('2x', EMPTY_STRING)
         .replaceAll('3X', EMPTY_STRING)
         .replaceAll('3x', EMPTY_STRING)
-        .replaceAll('4X', EMPTY_STRING)
-        .replaceAll('ƒ', 'â')
-        .replaceAll('╠ª', 'ț')
-        .replaceAll("'", '’')
-        .replaceAll('フ', 'ât')
-        .replaceAll('ハ', 'ân')
-        .replaceAll('Œ', 'î')
-        .replaceAll('á', 'ânt'),
-    );
-
-    const maybeNewFilePath = path.join(
-      __dirname,
-      process.env.VERIFIED_DIR,
-      `${maybeNewFileName}`,
+        .replaceAll('4X', EMPTY_STRING),
     );
 
     fs.unlinkSync(filePath);
-    fs.writeFileSync(maybeNewFilePath, fileContent);
+    fs.writeFileSync(path.join(__dirname, dir, newFileName), existingContent);
   });
 };
 
 (async () => {
-  reprocessFileNames(fs.readdirSync(process.env.VERIFIED_DIR));
+  reprocessFileNames(process.env.CANDIDATES_DIR);
+  reprocessFileNames(process.env.VERIFIED_DIR);
 })();
